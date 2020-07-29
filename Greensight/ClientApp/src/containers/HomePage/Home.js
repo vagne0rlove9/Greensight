8ae018ad850ab8f3e2b6bbd343a67b5fef884e21
vagne0rlove9 <kirik-pos@mail.ru>
2020-07-29 17:27:08 +0300

    add delivery

--- Greensight/ClientApp/src/containers/HomePage/Home.js
+++ Greensight/ClientApp/src/containers/HomePage/Home.js
@@ -1,11 +1,60 @@
-import React, { Component } from 'react';
+﻿import React, { useState } from 'react';
+import './Home.css'
 
-export class Home extends Component {
-  render () {
+export function Home() {
+    const [deliveryClass, setDeliveryClass] = useState("nav-buttons-active");
+    const [pickupClass, setPickupClass] = useState("nav-buttons");
+    const handleClickDelivery = () => {
+        if (deliveryClass === "nav-buttons") {
+            setDeliveryClass("nav-buttons-active");
+            setPickupClass("nav-buttons");
+        }
+    }
+    const handleClickPickup = () => {
+        if (pickupClass === "nav-buttons") {
+            setPickupClass("nav-buttons-active");
+            setDeliveryClass("nav-buttons");
+        }
+    }
     return (
-      <div>
-        
-      </div>
+        <>
+            <div className="div-delivery">
+                <div>
+                    Выберите способ доставки
+                </div>
+                <div onClick={handleClickDelivery} className={deliveryClass}>
+                    Доставка
+                </div>
+                <div onClick={handleClickPickup} className={pickupClass}>
+                    Самовывоз
+                </div>
+                {deliveryClass === "nav-buttons-active" ? 
+                <div className="input-container">
+                    <form>
+                        <div className="div-input-left">
+                            <label className="input-label">ФИО</label>
+                            <input className="input" placeholder="Только кириллица" />
+                        </div>
+                        <div className="div-input-right">
+                            <label className="input-label">Телефон</label>
+                            <input className="input" placeholder="+7(___) ___-__-__" />
+                        </div>
+                        <div className="div-input">
+                            <label className="input-label">Адрес доставки</label>
+                            <input className="input" placeholder="Город, улица, дом" />
+                        </div>
+                        <div className="div-input">
+                            <label className="input-label">Коментарий</label>
+                            <textarea className="input" rows={4} />
+                        </div>
+                        <div className="div-confirm-button">
+                            <button className="confirm-button">
+                                Оформить заказ
+                            </button>
+                        </div>
+                    </form>
+                </div> : null }
+            </div>
+        </>
     );
-  }
 }
