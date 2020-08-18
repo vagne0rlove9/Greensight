482ae91ae5141075ced5f8daad3c5f93e8c37fed
vagne0rlove9 <kirik-pos@mail.ru>
2020-08-18 12:46:10 +0300

    Updates

---
+++ precache-manifest.844cbd90d43cc5b550bc1c531b25ac39.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4b765f10d598a7645584246f4238a42",
+    "url": "/Greensight/index.html"
+  },
+  {
+    "revision": "602267c5f7a8ac60e42a",
+    "url": "/Greensight/static/css/2.74189dbb.chunk.css"
+  },
+  {
+    "revision": "cd783cc291d9f7f6e79e",
+    "url": "/Greensight/static/css/main.7b74f29b.chunk.css"
+  },
+  {
+    "revision": "602267c5f7a8ac60e42a",
+    "url": "/Greensight/static/js/2.e0431846.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/Greensight/static/js/2.e0431846.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd783cc291d9f7f6e79e",
+    "url": "/Greensight/static/js/main.4082ca62.chunk.js"
+  },
+  {
+    "revision": "e82acc87a12ff8c65bb4",
+    "url": "/Greensight/static/js/runtime-main.49cd7147.js"
+  }
+]);
